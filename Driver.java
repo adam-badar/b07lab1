8c7bb46a211434f3d957418a00b779219148c666
Adam Badar <badaradam10@gmail.com>
2022-10-03 15:58:15 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+//package geometry;
+
 public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
-  else 
-   System.out.println("1 is not a root of s"); 
- } 
-} 
+	 public static void main(String [] args) throws FileNotFoundException { 
+	  //Polynomial p = new Polynomial(); 
+	  //System.out.println(p.evaluate(3)); 
+	  double [] c1 = {2,-2, -2, 2};
+	  int [] e1 = {3, 2, 1, 0};  
+	  Polynomial p1 = new Polynomial(c1, e1); 
+	  double [] c2 = {-1, 1, 1, -1};
+	  int [] e2 = {3, 2, 1, 0};  
+	  Polynomial p2 = new Polynomial(c2, e2); 
+	  Polynomial s = p1.add(p2); 
+	  System.out.println("s(-1) = " + s.evaluate(1)); 
+	  if(s.hasRoot(1)) 
+		  System.out.println("1 is a root of s"); 
+	   else 
+		  System.out.println("1 is not a root of s");
+	  double [] c3 = {1, -2, 1};
+	  int [] e3 = {2, 1,0};
+ 	  Polynomial p3 = new Polynomial(c3, e3);
+ 	 double c4[] = {-1,-1234,-453,56,34,90};
+     int e4[] = {0,1,8,2,3,2};
+     Polynomial n = new Polynomial(c4,e4);
+     Polynomial k = p3.multiply(p2);
+//     File file = new File("C:\\Users\\badar\\OneDrive\\Desktop\\Java\\geometry\\random.txt");
+//     Polynomial f = new Polynomial(file);
+//     System.out.println("*******************");
+//     for(int i=0; i<f.coeff.length;i++)
+//     {
+//         System.out.println(f.coeff[i]+"\t"+f.expo[i]);
+//         //System.out.println(f.ex[i]);
+//     }
+//     f.saveToFile("do.txt");
+  
+     n.saveToFile("test.txt");
+	 } 
+	} 
--- Polynomial.java
+++ Polynomial.java
@@ -1,54 +1,175 @@
+//package geometry;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Collection;
 public class Polynomial {
-	double arr [];
+	double coeff [];
+	int expo [];
 	
 	public Polynomial() {
-		arr = new double[1];
-		this.arr[0] = 0;
+		coeff = new double[1];
+		this.coeff[0] = 0;
+		expo = new int[1];
+		this.expo[0] = 0;
 	}
 	
-	public Polynomial(double []a) {
-		arr = new double[a.length];
+	public Polynomial(double []a, int []b) {
+		coeff = new double[a.length];
+		expo = new int[b.length];
 		for(int i=0;i<a.length;i++) {
-			this.arr[i] = a[i];
+			this.coeff[i] = a[i];
+			this.expo[i] = b[i];
 		}
+	}
+	
+	public Polynomial(File file) throws FileNotFoundException {
+		
+		Scanner line = new Scanner(file);
+		String ok = line.nextLine();
+		int len = 1;
+		for(int i = 0; i < ok.length(); i++) {//gets the number of terms of the polynomial
+			if(ok.charAt(i) == '-' || ok.charAt(i) == '+') 
+				len++;
+		}
+		coeff = new double[len];
+		expo = new int[len];
+		
+		int temp1 = 0;
+		int temp2 = 0;
+		for(int i = 0; i < ok.length(); i++) {
+			//check the first character
+			while(i+1 < ok.length() && (ok.charAt(i+1) != '+' || ok.charAt(i+1) != '-' || ok.charAt(i) != 'x')) {
+				String no = "";
+				if(ok.charAt(0) == '-') {
+					no = no + "-";
+				}
+				for(int j = i+1; j != 'x'; j++) {
+					no = no + ok.charAt(j);
+				}
+				coeff[temp1] = Double.parseDouble(no);
+				temp1++;
+			}
+			if(ok.charAt(i) == '+' || ok.charAt(i) == '-') {//seperate the terms
+				String tem = "";
+				if(ok.charAt(i) == '-') {
+					tem = tem + "-";
+				}
+				for(int j = i+1; j != 'x'; j++) {//get the coeff
+					tem = tem + ok.charAt(j);
+				}
+				coeff[temp1] = Double.parseDouble(tem);
+				temp1++;
+			}
+			if(ok.charAt(i) == 'x') {//get the exponents
+				String tem = "";
+				for(int j = i+1; j != '+' || j != '-' || j != ok.length(); j++) {
+					tem = tem + ok.charAt(j);
+				}
+				expo[temp2] = Integer.parseInt(tem);
+				temp2++;
+			}
+		}
+		line.close();
+	}
+	
+	public void saveToFile(String filename) throws FileNotFoundException {
+		
+		String string = "";
+		for(int i = 0; i < coeff.length; i++) {
+			string = string + String.valueOf(coeff[i]);//add the coeffs
+			
+			if(expo[i] != 0) {
+				string = string + 'x';//add the varaible if nonzero expo
+				string = string + String.valueOf(expo[i]);//add the expo
+			}
+			if(i+1 < coeff.length && coeff[i+1] > 0) {
+				string = string + '+';//add the plus sign if positive
+			}
+		}
+		System.out.println(string);
+		
+		PrintStream aight = new PrintStream(filename);
+		aight.println(string);
+		aight.close();
 	}
 	
 	public Polynomial add(Polynomial other) {
-		int len = Math.min(this.arr.length, other.arr.length);
-		int len2 = Math.max(this.arr.length, other.arr.length);
-		double []naya = new double[len2];
-		Polynomial temp = new Polynomial(naya);
-		for(int i=0;i<len;i++) {
-			temp.arr[i] = other.arr[i] + this.arr[i];
+		HashMap<Integer, Double> lst = new HashMap<Integer, Double>();
+		for (int index = 0; index < expo.length; index++) {//add the existing arguments into a hashmap
+			lst.put(expo[index], coeff[index]);
 		}
-		if(other.arr.length > this.arr.length) {
-			for(int i=len;i < len2;i++) {
-				temp.arr[i] = temp.arr[i] + other.arr[i];
+		for (int i = 0; i < other.expo.length; i++) {
+			if (!lst.containsKey(other.expo[i])) {//if hashmap does not contain the key, add new pair
+				lst.put(other.expo[i], other.coeff[i]);
+			}
+			else{
+				Double doubl = lst.get(other.expo[i]) + other.coeff[i];//update the values for the key
+				lst.replace(other.expo[i], doubl);
 			}
 		}
-		else if (other.arr.length < this.arr.length) {
-			for(int i=len;i < len2;i++) {
-				temp.arr[i] = temp.arr[i] + this.arr[i];
-			}
+		Collection<Double> coeff2 = lst.values();
+		Object[] a = coeff2.toArray();
+		double [] d = new double [a.length];
+		for (int i = 0; i < a.length; i++) {//convert hashmap values (coeff) to array
+			d[i] = (Double)a[i];
 		}
-
-		return temp;
+		Collection<Integer> expo2 = lst.keySet();
+		a = expo2.toArray();
+		int [] b = new int [a.length];
+		for (int i = 0; i < a.length; i++) {//convert hashmap keys (exponents) to array
+			b[i] = (Integer)a[i];
+		}
+		return new Polynomial(d, b);
 	}
 	
 	public double evaluate(double a) {
 		double ret = 0;
-		for(int i=0;i<this.arr.length;i++) {
-			ret = this.arr[i]*Math.pow(a, i) + ret;	
+		for(int i=0;i<coeff.length;i++) {
+			ret = coeff[i]*Math.pow(a, expo[i]) + ret;	
 		}	
 		return ret;
 	}
 	
 	public boolean hasRoot(double a) {
-		if(evaluate(a) == 0) {
+		if(evaluate(a) == 0) 
 			return true;
+		return false;
+	}
+	
+	public Polynomial multiply(Polynomial other) {
+		HashMap<Integer, Double> lst = new HashMap<Integer, Double>();
+		//multiplies the exponents while adding the coeffs
+		for (int i = 0; i < other.expo.length; i++) { 
+			//iterate through each expo in other
+			for(int j = 0; j < expo.length; j++) {
+				//iterate through each expo in the hashmap
+				int k = other.expo[i] + expo[j];//add the exponents together
+				double val = other.coeff[i] * coeff[j];
+				
+				if(!lst.containsKey(k)) { //if the map doesn't contain the expo, insert it
+					lst.put(k, val);//add the new expo, multiply the coeffs
+				}
+				else {//if the expo alraedy exists, update the previous one by adding the new one
+					Double d = val + lst.get(k);
+					lst.replace(k, d);
+				}
+			}
 		}
-		else {
-			return false;
+		Collection<Double> coeff2 = lst.values();
+		Object[] a = coeff2.toArray();
+		double [] d = new double [a.length];
+		for (int i = 0; i < a.length; i++) {
+			d[i] = (Double)a[i];
 		}
+		Collection<Integer> expo2 = lst.keySet();
+		a = expo2.toArray();
+		int [] b = new int [a.length];
+		for (int i = 0; i < a.length; i++) {
+			b[i] = (Integer)a[i];
+		}
+		return new Polynomial(d, b);
 	}
 }
